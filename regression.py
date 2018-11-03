29a1c71899649d8e0033eb969bceffc9be6fedf6
holmen1 <holmen1@gmail.com>
2018-11-03 20:12:09 +0100

    pretty

--- regression.py
+++ regression.py
@@ -3,7 +3,6 @@ import numpy as np
 import quandl, math
 from sklearn import preprocessing, svm
 from sklearn.model_selection import train_test_split
-#from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
 
 
@@ -12,19 +11,14 @@ df = quandl.get("WIKI/GOOGL")
 #print(df.head())
 
 df = df[['Adj. Open',  'Adj. High',  'Adj. Low',  'Adj. Close', 'Adj. Volume']]
-
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100.0
-
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
-
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
 forecast_col = 'Adj. Close'
 df.fillna(value=-99999, inplace=True)
 forecast_out = int(math.ceil(0.01 * len(df)))
 df['label'] = df[forecast_col].shift(-forecast_out)
-
-#print(df.head())
 
 df.dropna(inplace=True)
 
