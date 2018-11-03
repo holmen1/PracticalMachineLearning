ebd10f0c5cfee07b6e24f4463546b58d134a160a
holmen1 <holmen1@gmail.com>
2018-11-03 20:06:47 +0100

    training and testing

--- regression.py
+++ regression.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import numpy as np
 import quandl, math
+from sklearn import preprocessing, svm
+from sklearn.model_selection import train_test_split
 #from sklearn import preprocessing, cross_validation, svm
-#
-#from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression
 
 
 df = quandl.get("WIKI/GOOGL")
 
-print(df.head())
+#print(df.head())
 
 df = df[['Adj. Open',  'Adj. High',  'Adj. Low',  'Adj. Close', 'Adj. Volume']]
 
@@ -23,4 +24,23 @@ df.fillna(value=-99999, inplace=True)
 forecast_out = int(math.ceil(0.01 * len(df)))
 df['label'] = df[forecast_col].shift(-forecast_out)
 
-print(df.head())
+#print(df.head())
+
+df.dropna(inplace=True)
+
+X = np.array(df.drop(['label'], 1))
+y = np.array(df['label'])
+
+X = preprocessing.scale(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+clf = svm.SVR()
+clf.fit(X_train, y_train)
+confidence = clf.score(X_test, y_test)
+print(confidence)
+
+clf = LinearRegression()
+clf.fit(X_train, y_train)
+confidence = clf.score(X_test, y_test)
+print(confidence)
