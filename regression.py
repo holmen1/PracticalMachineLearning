329d56c9e739fdbd856a9740fda5192b332a7aa0
holmen1 <holmen1@gmail.com>
2018-11-04 18:38:36 +0100

    p5 forecast and predict

--- regression.py
+++ regression.py
@@ -4,6 +4,9 @@ import quandl, math
 from sklearn import preprocessing, svm
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+import datetime
+import matplotlib.pyplot as plt
+from matplotlib import style
 
 
 df = quandl.get("WIKI/GOOGL")
@@ -20,21 +23,47 @@ df.fillna(value=-99999, inplace=True)
 forecast_out = int(math.ceil(0.01 * len(df)))
 df['label'] = df[forecast_col].shift(-forecast_out)
 
-df.dropna(inplace=True)
-
 X = np.array(df.drop(['label'], 1))
-y = np.array(df['label'])
-
 X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
 
+df.dropna(inplace=True)
+y = np.array(df['label'])
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-clf = svm.SVR()
+# less accurate
+# clf = svm.SVR()
+# clf.fit(X_train, y_train)
+# confidence = clf.score(X_test, y_test)
+# print(confidence)
+# confidence = 0.7924907633841987
+
+clf = LinearRegression(n_jobs=-1)
 clf.fit(X_train, y_train)
 confidence = clf.score(X_test, y_test)
 print(confidence)
 
-clf = LinearRegression()
-clf.fit(X_train, y_train)
-confidence = clf.score(X_test, y_test)
-print(confidence)
+forecast_set = clf.predict(X_lately)
+print(forecast_set, confidence, forecast_out)
+
+style.use('ggplot')
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += 86400
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
