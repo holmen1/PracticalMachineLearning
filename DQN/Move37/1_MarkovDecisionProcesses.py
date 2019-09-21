b0f68e3794a0e794fe9386c3f7798a9d786d78e5
Mats <holmen1@gmail.com>
2019-09-21 12:51:27 +0200

    MarkovDesicionProcesses

---
+++ DQN/Move37/1_MarkovDecisionProcesses.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import gym
+
+env = gym.make('CartPole-v0').unwrapped
+
+def run_episode(env, parameters):  
+    observation = env.reset()
+    totalreward = 0
+    for _ in range(200):
+        action = 0 if np.matmul(parameters,observation) < 0 else 1
+        observation, reward, done, info = env.step(action)
+        totalreward += reward
+        if done:
+            break
+    return totalreward
+
+parameters = np.random.rand(4) * 2 - 1
+bestparams = None  
+bestreward = 0  
+for _ in range(10000):  
+    parameters = np.random.rand(4) * 2 - 1
+    reward = run_episode(env,parameters)
+    print(reward)
+    if reward > bestreward:
+        bestreward = reward
+        bestparams = parameters
+        # considered solved if the agent lasts 200 timesteps
+        if reward == 200:
+            print('Solved!')
+            break
