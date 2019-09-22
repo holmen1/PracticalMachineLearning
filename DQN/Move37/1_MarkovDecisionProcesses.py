642f209270e7b9e80ae49f332e12248bcb2d2f7f
Mats <holmen1@gmail.com>
2019-09-22 20:09:10 +0200

    DynamicProgramming

--- DQN/Move37/1_MarkovDecisionProcesses.py
+++ DQN/Move37/1_MarkovDecisionProcesses.py
@@ -4,12 +4,17 @@ import gym
 
 env = gym.make('CartPole-v0').unwrapped
 
-def run_episode(env, parameters):  
+def run_episode(env, parameters):
+    print('parameters')
+    print(parameters)
     observation = env.reset()
     totalreward = 0
     for _ in range(200):
         action = 0 if np.matmul(parameters,observation) < 0 else 1
+        env.render()
         observation, reward, done, info = env.step(action)
+        print('observation')
+        print(observation)
         totalreward += reward
         if done:
             break
@@ -28,4 +33,17 @@ for _ in range(10000):
         # considered solved if the agent lasts 200 timesteps
         if reward == 200:
             print('Solved!')
+            break
+        
+noise_scaling = 1
+parameters = np.random.rand(4) * 2 - 1  
+bestreward = 0  
+for _ in range(10000):  
+    newparams = parameters + (np.random.rand(4) * 2 - 1)*noise_scaling
+    reward = run_episode(env,newparams)
+    if reward > bestreward:
+        bestreward = reward
+        parameters = newparams
+        if reward == 200:
+            print('Solved!!')
             break
